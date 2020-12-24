ebcb49333da096fb5adf714f17a2d81c5d8aa503
Bastiaan van Graafeiland <bastiaan89@live.nl>
2020-12-24 08:05:25 +0100

    day 4, part 2

--- src/day4.ts
+++ src/day4.ts
@@ -1,7 +1,39 @@
-export function numValidPassports(passports: string[]) {
+export function numCompletePassports(passports: string[]) {
     return passports.map(getPassportFields).filter((fields) => {
         return fields.filter(({ prop }) => prop !== 'cid').length === 7;
     }).length;
+}
+
+export function numValidPassports(passports: string[]) {
+    return passports.map(getPassportFields).filter((fields) => {
+        return fields.filter(validate).length === 7;
+    }).length;
+}
+
+const validate = ({ prop, value }: { prop: string, value: string }) => {
+    switch (prop) {
+        case 'byr':
+            const birthYear = parseInt(value);
+            return birthYear >= 1920 && birthYear <= 2002;
+        case 'iyr':
+            const issueYear = parseInt(value);
+            return issueYear >= 2010 && issueYear <= 2020;
+        case 'eyr':
+            const expirationYear = parseInt(value);
+            return expirationYear >= 2020 && expirationYear <= 2030;
+        case 'hgt':
+            const parts = value.match(/(\d+)(in|cm)/);
+            if (!parts) return false;
+            const number = parseInt(parts[1]);
+            return parts[2] === 'cm' ? number >= 150 && number <= 193 : number >= 59 && number <= 76;
+        case 'hcl':
+            return RegExp(/^#[a-f0-9]{6}$/).test(value);
+        case 'ecl':
+            return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value);
+        case 'pid':
+            return RegExp(/^\d{9}$/).test(value);
+    }
+    return false;
 }
 
 function getPassportFields(passport: string) {
--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,28 @@ import { productOfPairs, productOfTriples } from "./day1";
 import { getLines, getNumbers, parsePassportInput } from "./utilities";
 import { isValidOfficialPassword, isValidSledRentalPassword, numValidPasswords } from "./day2";
 import { multiplyEncounteredTrees, numTreesEncountered } from "./day3";
-import { numValidPassports } from "./day4";
-//
-// fs.readFile('./input/day1.txt').then((contents) => {
-//     let numbers = getNumbers(contents.toString());
-//     console.log(`day 1, part 1: ${productOfPairs(numbers)}`);
-//     console.log(`day 1, part 2: ${productOfTriples(numbers)}`);
-// });
-//
-// fs.readFile('./input/day2.txt').then((contents) => {
-//     let passwords = getLines(contents.toString());
-//     console.log(`day 2, part 1: ${numValidPasswords(passwords, isValidSledRentalPassword)}`);
-//     console.log(`day 2, part 2: ${numValidPasswords(passwords, isValidOfficialPassword)}`);
-// });
-//
-// fs.readFile('./input/day3.txt').then((contents) => {
-//     let map = getLines(contents.toString()).map((line) => line.split(''));
-//     console.log(`day 3, part 1: ${numTreesEncountered(map, 3, 1)}`);
-//     console.log(`day 3, part 2: ${multiplyEncounteredTrees(map)}`);
-// });
+import { numCompletePassports, numValidPassports } from "./day4";
+
+fs.readFile('./input/day1.txt').then((contents) => {
+    let numbers = getNumbers(contents.toString());
+    console.log(`day 1, part 1: ${productOfPairs(numbers)}`);
+    console.log(`day 1, part 2: ${productOfTriples(numbers)}`);
+});
+
+fs.readFile('./input/day2.txt').then((contents) => {
+    let passwords = getLines(contents.toString());
+    console.log(`day 2, part 1: ${numValidPasswords(passwords, isValidSledRentalPassword)}`);
+    console.log(`day 2, part 2: ${numValidPasswords(passwords, isValidOfficialPassword)}`);
+});
+
+fs.readFile('./input/day3.txt').then((contents) => {
+    let map = getLines(contents.toString()).map((line) => line.split(''));
+    console.log(`day 3, part 1: ${numTreesEncountered(map, 3, 1)}`);
+    console.log(`day 3, part 2: ${multiplyEncounteredTrees(map)}`);
+});
 
 fs.readFile('./input/day4.txt').then((contents) => {
     let passports = parsePassportInput(contents.toString());
-    console.log(`day 4, part 1: ${numValidPassports(passports)}`);
+    console.log(`day 4, part 1: ${numCompletePassports(passports)}`);
+    console.log(`day 4, part 2: ${numValidPassports(passports)}`);
 });
